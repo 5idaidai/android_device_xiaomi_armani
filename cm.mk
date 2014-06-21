b3b48ade895bd680275ade629f0b9139563a41ff
utopiar <utopiar@utopia.(none)>
2014-06-21 11:29:32 +0800

    mkvendor.sh: Initial commit of armani

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := armani
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/armani/device_armani.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := armani
+PRODUCT_NAME := cm_armani
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := armani
+PRODUCT_MANUFACTURER := xiaomi
