a4f862d49c219968c8da870f64dbace7c4c9b04e
utopiar <zhengdaxg@163.com>
2014-07-06 14:48:09 +0800

    add prebuild boot.img-dt and change the ramdisk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,9 +18,14 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := armani
 
+# add boot.img-bt arg
+BOARD_MKBOOTIMG_ARGS += --dt device/xiaomi/armani/prebuilt/boot.img-dt
+
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_DT_SIZE := 5605376
+
 
 # fix this up by examining /proc/mtd on a running device
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -30,7 +35,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838860800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5905580032
 BOARD_FLASH_BLOCK_SIZE := 32768
 
-TARGET_PREBUILT_KERNEL := device/xiaomi/armani/kernel
+TARGET_PREBUILT_KERNEL := device/xiaomi/armani/prebuilt/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
@@ -51,4 +56,4 @@ DEVICE_RESOLUTION := 720x1280
 TW_IGNORE_MAJOR_AXIS_0 := true
 
 # userdata 8GB
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 8589934592
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 8589934592
--- device_armani.mk
+++ device_armani.mk
@@ -9,7 +9,7 @@ DEVICE_PACKAGE_OVERLAYS += device/xiaomi/armani/overlay
 
 LOCAL_PATH := device/xiaomi/armani
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -20,9 +20,28 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 
-# libwebcore
-#PRODUCT_COPY_FILES := \
-#    device/xiaomi/armani/lib/libwebviewchromium.so:system/lib/#libwebviewchromium.so
+# Ramdisk files
+PRODUCT_COPY_FILES += \
+    device/xiaomi/armani/ramdisk/fstab.qcom:root/fstab.qcom \
+    device/xiaomi/armani/ramdisk/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.class_main.sh:root/init.qcom.class_main.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.early_boot.sh:root/init.qcom.early_boot.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.factory.sh:root/init.qcom.factory.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.rc:root/init.qcom.rc \
+    device/xiaomi/armani/ramdisk/init.qcom.sh:root/init.qcom.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.ril.sh:root/init.qcom.ril.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.ssr.sh:root/init.qcom.ssr.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.syspart_fixup.sh:root/init.qcom.syspart_fixup.sh \
+    device/xiaomi/armani/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
+    device/xiaomi/armani/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
+    device/xiaomi/armani/ramdisk/init.target.rc:root/init.target.rc \
+    device/xiaomi/armani/ramdisk/ueventd.qcom.rc:root/ueventd.qcom.rc \
+    device/xiaomi/armani/ramdisk/sbin/chargeonlymode:root/sbin/chargeonlymode \
+    device/xiaomi/armani/ramdisk/default.prop:root/default.prop \
+    device/xiaomi/armani/ramdisk/file_contexts:root/file_contexts \
+    device/xiaomi/armani/ramdisk/property_contexts:root/property_contexts \
+    device/xiaomi/armani/ramdisk/seapp_contexts:root/seapp_contexts \
+    device/xiaomi/armani/ramdisk/sepolicy:root/sepolicy 
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_armani
